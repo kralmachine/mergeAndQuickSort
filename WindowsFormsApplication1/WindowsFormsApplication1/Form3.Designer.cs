f3119133dcc0386f6a17c800dd5891901c590bf7
ABDULLAH AVŞAR <abdullahavs92@gmail.com>
2017-06-13 14:19:26 +0200

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form3.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDizi10000 = new System.Windows.Forms.Button();
+            this.btnDizi1000 = new System.Windows.Forms.Button();
+            this.btnDizi100 = new System.Windows.Forms.Button();
+            this.btnDizi10 = new System.Windows.Forms.Button();
+            this.btnDiziSiralama = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDizi10000
+            // 
+            this.btnDizi10000.Location = new System.Drawing.Point(12, 124);
+            this.btnDizi10000.Name = "btnDizi10000";
+            this.btnDizi10000.Size = new System.Drawing.Size(146, 23);
+            this.btnDizi10000.TabIndex = 9;
+            this.btnDizi10000.Text = "DİZİ 10000";
+            this.btnDizi10000.UseVisualStyleBackColor = true;
+            this.btnDizi10000.Click += new System.EventHandler(this.btnDizi10000_Click);
+            // 
+            // btnDizi1000
+            // 
+            this.btnDizi1000.Location = new System.Drawing.Point(12, 95);
+            this.btnDizi1000.Name = "btnDizi1000";
+            this.btnDizi1000.Size = new System.Drawing.Size(146, 23);
+            this.btnDizi1000.TabIndex = 8;
+            this.btnDizi1000.Text = "DİZİ 1000";
+            this.btnDizi1000.UseVisualStyleBackColor = true;
+            this.btnDizi1000.Click += new System.EventHandler(this.btnDizi1000_Click);
+            // 
+            // btnDizi100
+            // 
+            this.btnDizi100.Location = new System.Drawing.Point(12, 66);
+            this.btnDizi100.Name = "btnDizi100";
+            this.btnDizi100.Size = new System.Drawing.Size(146, 23);
+            this.btnDizi100.TabIndex = 7;
+            this.btnDizi100.Text = "DİZİ 100";
+            this.btnDizi100.UseVisualStyleBackColor = true;
+            this.btnDizi100.Click += new System.EventHandler(this.btnDizi100_Click);
+            // 
+            // btnDizi10
+            // 
+            this.btnDizi10.Location = new System.Drawing.Point(12, 36);
+            this.btnDizi10.Name = "btnDizi10";
+            this.btnDizi10.Size = new System.Drawing.Size(146, 23);
+            this.btnDizi10.TabIndex = 6;
+            this.btnDizi10.Text = "DİZİ 10";
+            this.btnDizi10.UseVisualStyleBackColor = true;
+            this.btnDizi10.Click += new System.EventHandler(this.btnDizi10_Click);
+            // 
+            // btnDiziSiralama
+            // 
+            this.btnDiziSiralama.Location = new System.Drawing.Point(12, 6);
+            this.btnDiziSiralama.Name = "btnDiziSiralama";
+            this.btnDiziSiralama.Size = new System.Drawing.Size(146, 23);
+            this.btnDiziSiralama.TabIndex = 5;
+            this.btnDiziSiralama.Text = "DİZİ SIRALAMA";
+            this.btnDiziSiralama.UseVisualStyleBackColor = true;
+            this.btnDiziSiralama.Click += new System.EventHandler(this.btnDiziSiralama_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(170, 152);
+            this.Controls.Add(this.btnDizi10000);
+            this.Controls.Add(this.btnDizi1000);
+            this.Controls.Add(this.btnDizi100);
+            this.Controls.Add(this.btnDizi10);
+            this.Controls.Add(this.btnDiziSiralama);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ALGORİTMA 3.ÖDEV";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDizi10000;
+        private System.Windows.Forms.Button btnDizi1000;
+        private System.Windows.Forms.Button btnDizi100;
+        private System.Windows.Forms.Button btnDizi10;
+        private System.Windows.Forms.Button btnDiziSiralama;
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form3.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        #region tanimlama
+        ///////////////////////////////
+        int counter = 0;
+        int[] A;
+        int sayininSayisi = 0;
+        Random rnd = new Random();
+        StringBuilder sb = new StringBuilder();
+        Stopwatch time = new Stopwatch();
+        DialogResult algoritmaSec;
+        //////////////////////////////
+        #endregion
+        private void verileriDoldur(int gelenSayi)
+        {
+            sb.Clear();
+            if (sayininSayisi == 10)
+            {
+                sb.AppendLine("DİZİNİN SIRALANMAMIŞ HALİ");
+                sb.AppendLine("---------------------------");
+            }
+
+            for (int i = 0; i < gelenSayi; i++)
+            {
+                A[i] = rnd.Next(0, 10000);
+                if (sayininSayisi == 10)
+                    sb.AppendLine(A[i].ToString() + "");
+            }
+        }
+
+        private int quickSort(int[] A, int p, int r,int counter)
+        {
+            int q;
+            counter++;
+            if (p < r)
+            {
+                counter++;
+                q = partition(A, p, r, ref counter);
+                counter++;
+                quickSort(A, p, q - 1, counter);
+                counter++;
+                this.counter=quickSort(A, q + 1, r, counter);
+                counter++;
+            }
+            else
+                counter++;
+            return counter;
+        }
+        private  int partition(int[] A, int p, int r,ref int counter)
+        {
+            int tmp;
+            counter++;
+            int x = A[r];
+            counter++;
+            int i = p - 1;
+            counter++;
+            for (int j = p; j <= r - 1;counter++,j++)
+            {
+                counter++;
+                if (A[j] <= x)
+                {
+                    counter++;
+                    i++;
+                    counter++;
+                    tmp = A[i];
+                    counter++;
+                    A[i] = A[j];
+                    counter++;
+                    A[j] = tmp;
+                    counter++;
+                }
+                else
+                    counter++;
+            }
+            tmp = A[i + 1];
+            counter++;
+            A[i + 1] = A[r];
+            counter++;
+            A[r] = tmp;
+            counter++;
+            return i + 1;
+          
+        }
+
+        private int sort(int[] arr, int l, int r,int counter)
+        {
+            if (l < r)
+            {
+                counter++;
+                int m = (l + r) / 2;
+                counter++;
+                sort(arr, l, m,counter);
+                counter++;
+                this.counter=sort(arr, m + 1, r,counter);
+                counter++;
+                merge(arr, l, m, r,ref counter);
+                counter++;
+            }
+            else
+                counter++;
+            return counter;
+
+        }
+        private void merge(int[] arr, int l, int m, int r,ref int counter)
+        {
+            int n1 = m - l + 1;
+            counter++;
+            int n2 = r - m;
+            counter++;
+            int[] L = new int[n1];
+            counter++;
+            int[] R = new int[n2];
+            counter++;
+            for (int h = 0; h < n1; counter++, ++h)
+            {
+                counter++;
+                L[h] = arr[l + h];
+                counter++;
+            }
+            for (int g = 0; g < n2; counter++, ++g)
+            {
+                counter++;
+                R[g] = arr[m + 1 + g];
+                counter++;
+            }
+            int i = 0;
+            counter++;
+            int j = 0;
+            counter++;
+
+            int k = l;
+            counter++;
+            while (i < n1 && j < n2)
+            {
+                counter++;
+                if (L[i] <= R[j])
+                {
+                    counter++;
+                    arr[k] = L[i];
+                    counter++;
+                    i++;
+                    counter++;
+                }
+                else if(L[i]>=R[j])
+                {
+                    counter++;
+                    arr[k] = R[j];
+                    counter++;
+                    j++;
+                    counter++;
+                }
+                k++;
+                counter++;
+            }
+
+            while (i < n1)
+            {
+                counter++;
+                arr[k] = L[i];
+                counter++;
+                i++;
+                counter++;
+                k++;
+                counter++;
+            }
+
+            while (j < n2)
+            {
+                counter++;
+                arr[k] = R[j];
+                counter++;
+                j++;
+                counter++;
+                k++;
+                counter++;
+            }
+        }
+
+
+        private void btnDiziSiralama_Click(object sender, EventArgs e)
+        {
+            algoritmaSec = MessageBox.Show("MERGE SORT SEÇMEK İÇİN EVET QUICK SORT SEÇMEK İÇİN HAYIR SEÇİNİZ.", "SORU", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            algoritmalar(algoritmaSec);
+        }
+        private void algoritmalar(DialogResult algoSonuc)
+        {
+            if (algoSonuc == DialogResult.Yes)
+            {
+                time.Reset();
+                
+                //A[10] durumu
+                A = new int[10];
+                sayininSayisi = 10;
+                verileriDoldur(sayininSayisi);
+                sb.AppendLine("\nDİZİNİN SIRALANMIŞ HALİ");
+                sb.AppendLine("---------------------------");
+                /////////////////////////
+                time.Start();
+                sort(A, 0, A.Length - 1,counter);
+                //////////////////////////
+                time.Stop();
+                MessageBox.Show((time.Elapsed) + " " + "mikrosaniyede işlem yapmıştır." + " Counter Değeri :" + counter, "MERGE SORT");
+                for (int i = 0; i < 10; i++)
+                    sb.AppendLine(A[i].ToString());
+                MessageBox.Show(sb.ToString(), "MERGE SORT");
+            }
+            else
+            {
+                time.Reset();
+                
+                //A[10] durumu
+                A = new int[10];
+                sayininSayisi = 10;
+                verileriDoldur(sayininSayisi);
+                sb.AppendLine("\nDİZİNİN SIRALANMIŞ HALİ");
+                sb.AppendLine("---------------------------");
+                /////////////////////////
+                time.Start();
+                quickSort(A, 0, A.Length - 1,counter);
+                //////////////////////////
+                time.Stop();
+                MessageBox.Show((time.Elapsed) + " " + "mikrosaniyede işlem yapmıştır." + " Counter Değeri :" + counter, "QUICK SORT");
+                for (int i = 0; i < 10; i++)
+                    sb.AppendLine(A[i].ToString());
+                MessageBox.Show(sb.ToString(), "QUICK SORT");
+            }
+
+            counter = 0;
+            sayininSayisi = 0;
+        }
+        private void btnDizi10_Click(object sender, EventArgs e)
+        {
+            //A[10] durumu için counter ve mikrosaniye değeri
+            A = new int[10];
+            sayininSayisi = 10;
+            verileriDoldur(10);
+            algoritmaSec = MessageBox.Show("MERGE SORT SEÇMEK İÇİN EVET QUICK SORT SEÇMEK İÇİN HAYIR SEÇİNİZ.", "SORU", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (algoritmaSec == DialogResult.Yes)
+            {
+                time.Reset();
+                time.Start();
+                sort(A, 0, A.Length - 1,counter);
+                time.Stop();
+                MessageBox.Show((time.Elapsed) + " " + "mikrosaniyede işlem yapmıştır." + " Counter Değeri :" + counter, "MERGE SORT");
+            }
+            else
+            {
+                time.Reset();
+                time.Start();
+                quickSort(A, 0, A.Length - 1,counter);
+                time.Stop();
+                MessageBox.Show((time.Elapsed) + " " + "mikrosaniyede işlem yapmıştır." + " Counter Değeri :" + counter, "QUICK SORT");
+            }
+            counter = 0;
+            sayininSayisi = 0;
+
+        }
+        private void btnDizi100_Click(object sender, EventArgs e)
+        {
+            //A[100] durumu için counter ve mikrosaniye değeri
+            A = new int[100];
+            verileriDoldur(100);
+            sayininSayisi = 100;
+            algoritmaSec = MessageBox.Show("MERGE SORT SEÇMEK İÇİN EVET QUICK SORT SEÇMEK İÇİN HAYIR SEÇİNİZ.", "SORU", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (algoritmaSec == DialogResult.Yes)
+            {
+                time.Reset();
+                time.Start();
+                sort(A, 0, A.Length - 1,counter);
+                time.Stop();
+                MessageBox.Show((time.Elapsed) + " " + "mikrosaniyede işlem yapmıştır." + " Counter Değeri :" + counter, "MERGE SORT");
+            }
+            else
+            {
+                time.Reset();
+                time.Start();
+                quickSort(A, 0, A.Length - 1,counter);
+                time.Stop();
+                MessageBox.Show((time.Elapsed) + " " + "mikrosaniyede işlem yapmıştır." + " Counter Değeri :" + counter, "QUICK SORT");
+            }
+            counter = 0;
+            sayininSayisi = 0;
+        }
+        private void btnDizi1000_Click(object sender, EventArgs e)
+        {
+            //A[1000] durumu için counter ve mikrosaniye değeri
+            A = new int[1000];
+            verileriDoldur(1000);
+            sayininSayisi = 1000;
+            algoritmaSec = MessageBox.Show("MERGE SORT SEÇMEK İÇİN EVET QUICK SORT SEÇMEK İÇİN HAYIR SEÇİNİZ.", "SORU", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (algoritmaSec == DialogResult.Yes)
+            {
+                time.Reset();
+                time.Start();
+                sort(A, 0, A.Length - 1,counter);
+                time.Stop();
+                MessageBox.Show((time.Elapsed) + " " + "mikrosaniyede işlem yapmıştır." + " Counter Değeri :" + counter, "MERGE SORT");
+            }
+            else
+            {
+                time.Reset();
+                time.Start();
+                quickSort(A, 0, A.Length - 1,counter);
+                time.Stop();
+                MessageBox.Show((time.Elapsed) + " " + "mikrosaniyede işlem yapmıştır." + " Counter Değeri :" + counter, "QUICK SORT");
+            }
+            counter = 0;
+            sayininSayisi = 0;
+        }
+        private void btnDizi10000_Click(object sender, EventArgs e)
+        {
+            //A[10000] durumu için counter ve mikrosaniye değeri
+            A = new int[10000];
+            verileriDoldur(10000);
+            sayininSayisi = 10000;
+            algoritmaSec = MessageBox.Show("MERGE SORT SEÇMEK İÇİN EVET QUICK SORT SEÇMEK İÇİN HAYIR SEÇİNİZ.", "SORU", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (algoritmaSec == DialogResult.Yes)
+            {
+                time.Reset();
+                time.Start();
+                sort(A, 0, A.Length - 1,counter);
+                time.Stop();
+                MessageBox.Show((time.Elapsed) + " " + "mikrosaniyede işlem yapmıştır." + " Counter Değeri :" + counter, "MERGE SORT");
+            }
+            else
+            {
+                time.Reset();
+                time.Start();
+                quickSort(A, 0, A.Length - 1,counter);
+                time.Stop();
+                MessageBox.Show((time.Elapsed) + " " + "mikrosaniyede işlem yapmıştır." + " Counter Değeri :" + counter, "QUICK SORT");
+            }
+            counter = 0;
+            sayininSayisi = 0;
+        }
+    }
+}
